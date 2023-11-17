7dc9d5358e8c127e5008b2999e89d4e3219899a5
Echo914 <fortuneklabi6@gmail.com>
2023-11-17 15:08:37 +0000

    added notes and routines sections

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,3 @@
-import VanillaCalendar from '@uvarov.frontend/vanilla-calendar';
-import '@uvarov.frontend/vanilla-calendar/build/vanilla-calendar.min.css';
-import '@uvarov.frontend/vanilla-calendar/build/themes/dark.min.css';
-
-
 function activate (elmnt) {
     var headers = document.getElementsByClassName("nav-btn");
     for(i = 0; i < headers.length; i++) {
@@ -15,13 +10,62 @@ function activate (elmnt) {
     elmnt.classList.add("active");
 }
 
-const options = {
-    settings: {
-      visibility: {
-        theme: 'light',
-      },
-    },
-  };
-  
-  const calendar = new VanillaCalendar('#calendar', options);
-  calendar.init();
+// Gets random integers b/n 0 and 255 to create random rgb colors 
+function get_random_color(diminish=1) {
+  var red = Math.floor(Math.random() * 256); // Random red
+  var green = Math.floor(Math.random() * 256); // Random green
+  var blue = Math.floor(Math.random() * 256); // Random blue
+
+  var random_color = `rgba(${red}, ${green}, ${blue}, ${diminish})`;
+  return random_color;
+}
+
+// // Toggles display of overlay/curtain 
+// function openCurtain(curtain, form) {
+//   document.getElementById(curtain).style.width = "100%";
+//   document.getElementById(form).style.display = "1";
+// }
+
+// function closeCurtain(curtain, form) {
+//   document.getElementById(curtain).style.width = "0%";
+//   document.getElementById(form).style.display = "none";
+// }
+
+// Toggles display of overlay/curtain 
+function openCurtain(curtain, form) {
+  document.getElementById(curtain).style.width = "100%";
+  setTimeout(() => {
+    document.getElementById(form).style.opacity = "1";
+  }, 200);
+}
+
+function closeCurtain(curtain, form) {
+  document.getElementById(form).style.opacity = "0";
+  setTimeout(() => {
+    document.getElementById(curtain).style.width = "0";
+  }, 400);
+}
+
+// Applies a random color to the background of each item in task_divs list
+var task_divs = document.querySelectorAll(".task-item");
+var task_content_list = document.querySelectorAll(".task-content");
+
+task_divs.forEach((task) => {
+  let random_color = get_random_color(0.4);
+  task.style.backgroundColor = random_color;
+})
+
+setTimeout(() => {
+  task_content_list.forEach((task_content) => {
+    task_content.style.opacity = "1";
+  })
+}, 1000);
+
+
+// Applies a random color to routine items
+var routine_divs = document.querySelectorAll(".routine-details");
+routine_divs.forEach((routine_div) => {
+  var random_color = get_random_color(0.4);
+  routine_div.style.borderTop = `3px outset ${random_color}`
+  routine_div.style.backgroundColor = random_color;
+});
